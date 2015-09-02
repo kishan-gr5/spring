97dd51712d2c8db54ac749002d8a684d55ea1256
Kishan G R <kishan.g.r@gmail.com>
2015-09-02 18:14:26 +0530

    sefdg

---
+++ SpringDemo/src/com/spring/kishan/DrawingApp.java
@@ -0,0 +1,19 @@
+package com.spring.kishan;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("Spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+			Triangle triangle = (Triangle)context.getBean("triangle");
+			triangle.draw();
+	}
+
+}
